c1d7a585255fd4c8a01933dbe117fc4c9328f2b7
jnferguson <jf@ownco.net>
2014-11-02 01:55:31 -0500

    Create app.c
    note the use of /dev/NIBBLER4... if you don't create at least 4 devices, this of course will fail. That is okay, its not really meant to be used.

---
+++ linux/src/app.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include <nbd/ioctl.h>
+
+signed int
+main(void)
+{
+    char*           dev = "/dev/NIBBLER4";
+    signed int      fd  = open(dev, O_RDWR);
+    ip_network_t    net = {0};
+    uint64_t        len = 0;
+    void*           ptr = NULL;
+
+    if (0 > fd) {
+        perror("open()");
+        return EXIT_FAILURE;
+    }
+
+    net.type        = TYPE_IPV4;
+    net.net.ipv4    = 0x7FFF0001;
+    net.mask        = 8;
+    net.dport       = 1;
+
+    if (0 != ioctl(fd, SET_IP_NETWORK_IOCTL, &net)) {
+        perror("ioctl(SET_IP_NETWORK_IOCTL)");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    if (0 != ioctl(fd, GET_DATA_LENGTH_IOCTL, &len)) {
+        perror("ioctl(GET_DATA_LENGTH_IOCTL)");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    ptr = mmap(NULL, len, PROT_NONE, MAP_SHARED, fd, 0);
+
+    if (MAP_FAILED == ptr) {
+        perror("mmap()");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    printf("[%u] ptr: %p\n", getpid(), ptr);
+    sleep(60*5);
+    close(fd);
+    return EXIT_SUCCESS;
+
+}
